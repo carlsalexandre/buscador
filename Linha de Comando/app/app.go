1af279af728aa1bc3029d97ee3d8696d09a39954
carlsalexandre <contatocarlosalexandrecb@gmail.com>
2026-07-06 02:52:31 -0300

    Add IP lookup command
    Add a new 'ip' command to the CLI application that performs DNS hostname resolution. The command accepts a 'host' flag (defaults to amazon.com.br) and displays all resolved IP addresses using net.LookupIP.

--- Linha de Comando/app/app.go
+++ Linha de Comando/app/app.go
@@ -1,11 +1,43 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de endereços web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "amazon.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
